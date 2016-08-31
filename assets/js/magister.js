9acb18e726ca86ecfc8376af32085abc792bd280
aimablegon <aimablegon@gmail.com>
2016-08-31 16:36:20 +0900

    16.08.31-16:30

--- assets/js/magister.js
+++ assets/js/magister.js
@@ -1,4 +1,4 @@
-// global. currently active menu item 
+// global. currently active menu item
 var current_item = 0;
 
 // few settings
@@ -11,31 +11,31 @@ $(window).load(function() {
 	//====================================//
 	//			nav click event			  //
 	//====================================//
-	
-	
+
+
 	$('.mainmenu ul li a').click(function(){
-		if( ! $(this).hasClass('active') ) { 
+		if( ! $(this).hasClass('active') ) {
 			$(this).addClass('active').parent().siblings().children('a').removeClass('active');
 		}
 		return false;
-	});	
-	
+	});
+
 	$('.mainmenu .container .dropdown>button').click(function(){
 		// console.log('aa')
 		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		
+
 		$(this).animate({
 			top:'-200px'
 		},300)
-		
+
 		$('.mainmenu .container .dropdown>.gnb_menu').animate({
 			top:0
 		}, 500).addClass('animated bounceInDown').one(animationEnd, function(){
         $(this).removeClass('animated bounceInDown');
     	});
 	})
-	
-	
+
+
 	$('.mainmenu .gnb_menu .btn_close').click(function(){
 		$(this).parents('.gnb_menu').animate({
 			top:'-200px'
@@ -44,21 +44,21 @@ $(window).load(function() {
 			top:0
 		},200)
 	})
-	
-	
-	
+
+
+
 	//====================================//
 	//			scroll Event			  //
 	//====================================//
 		var spot = []
 	    $('.wrapper>section').each(function(){
-	        
+
 	        spot.push($(this).offset().top);
 	        console.log(spot);
-	        
+
 	    })
-	    
-	    
+
+
 	    $('.dropdown li a').click(function(){
 	        // console.log('ssss')
 	        var idx = $(this).parent().index();
@@ -66,18 +66,18 @@ $(window).load(function() {
 	          scrollTop:spot[idx]
 	        })
 	        return false;
-	        
-	    }) 
-	  
+
+	    })
+
 	//====================================//
 	//		next&prev click Event		  //
 	//====================================//
-	
+
 	var idx = 0;
 	var slideWidth = $('.port').width()
-	$('.port .next a').click(function(){
+	$('.port .next').click(function(){
 		console.log('aa')
-		
+
 		$('.slide .slide_port>div').eq(idx).removeClass('on').children('div').animate({
 			'left':-slideWidth
 		}).parent('div').next().addClass('on').children('div').css({
@@ -86,9 +86,9 @@ $(window).load(function() {
 		}).stop().animate({
             left:'10%'
         })
-        
+
         idx++
-                
+
         if(idx == $('.slide .slide_port>div').length){
             idx =0
             $('.slide .slide_port>div').eq(idx).children('div').css({
@@ -99,9 +99,9 @@ $(window).load(function() {
         }
         return false
 	})
-	
-	$('.port .prev a').click(function(){
-                
+
+	$('.port .prev').click(function(){
+
         $('.slide .slide_port>div').eq(idx).removeClass('on').children('div').animate({
                 left:slideWidth
             }).parent('div').prev().addClass('on').children('div').css({
@@ -110,13 +110,13 @@ $(window).load(function() {
             }).stop().animate({
                 left:'10%'
             })
-            
-            
+
+
             idx--
             // console.log(idx)
         if(idx <0){
             idx = $('.slide .slide_port>div').length-1;
-            
+
             // console.log(idx)
             $('.slide .slide_port>div').eq(idx).children('div').css({
                 left:-slideWidth,
@@ -127,7 +127,46 @@ $(window).load(function() {
         }
         return false
     })
-        
+
+	//====================================//
+	//				star Event			  //
+	//====================================//
+
+	$('html').jstars({
+		image_path: 'assets/images',
+		style: 'white',
+		image: 'jstar-map.png',
+	
+		frequency: 17,
+		delay: 600
+
+
+	});
+	//====================================//
+	//	   	 l'outil mouse Event          //
+	//====================================//
+
+	$('.skill .sk_li span:first-child').mouseenter(function(){
+		$(this).css({
+			'background-color':'black'
+		}).children('img').css({
+			'display':'none',
+
+		}).parent().next('span').css({
+			'display':'block'
+		})
+	}).mouseleave(function(){
+		$(this).css({
+			'background-color':'transparent'
+		}).children('img').css({
+			'display':'block'
+		}).parent().next('span').css({
+			'display':'none'
+		})
+	})
+
+
+
 });
 
 //
